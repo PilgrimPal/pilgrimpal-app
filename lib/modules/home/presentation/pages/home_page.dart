ba3446afaa26804f4dc99ecde6f7a654265213b7
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2024-02-08 11:28:44 +0700

    feat: create drawer and initial home page

---
+++ lib/modules/home/presentation/pages/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:pilgrimpal_app/common/widgets/app_drawer.dart';
+
+class HomePage extends StatelessWidget {
+  static const routeName = "/home";
+
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("PilgrimPal")),
+      drawer: const AppDrawer(),
+    );
+  }
+}
---
+++ lib/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:pilgrimpal_app/modules/home/presentation/pages/home_page.dart';
+
+const homeRoute = "/home";
+
+final routes = {
+  homeRoute: (BuildContext context) => const HomePage(),
+};
