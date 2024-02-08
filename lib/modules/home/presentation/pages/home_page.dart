f71bb995f99221743e6f019c6cd9044447802520
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2024-02-08 12:01:58 +0700

    feat: initialize home page cards

--- lib/modules/home/presentation/pages/home_page.dart
+++ lib/modules/home/presentation/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pilgrimpal_app/modules/common/widgets/app_drawer.dart';
 
 class HomePage extends StatelessWidget {
   static const routeName = "/home";
@@ -10,7 +9,54 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("PilgrimPal")),
-      drawer: const AppDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 4,
+          children: const [
+            Card(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.people,
+                      size: 60,
+                      color: Colors.deepPurple,
+                    ),
+                    Text(
+                      "Crowdness",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.chat,
+                      size: 60,
+                      color: Colors.deepPurple,
+                    ),
+                    Text(
+                      "Assistant",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
